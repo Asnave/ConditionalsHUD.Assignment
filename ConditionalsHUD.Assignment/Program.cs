081c350f20dfae8db1d9f5a781d6c662eef78b16
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-10-04 13:25:56 -0300

    take damage, heal

--- ConditionalsHUD.Assignment/Program.cs
+++ ConditionalsHUD.Assignment/Program.cs
@@ -8,8 +8,41 @@ namespace ConditionalsHUD.Assignment
 {
     class Program
     {
+        string ;
+        static int health = 100;
+        static int damage = -20;
+        static int hp = +10;
+
+        static void ChangeWeapon()
+        {
+
+        }
+
+        static void TakeDamage()
+        {
+            health = health + damage;
+        }
+
+        static void Heal()
+        {
+            health = health + hp;
+        }
+        static void ShowHUD()
+        {
+            Console.WriteLine("------HUD------");
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Current Weapon: ");
+            Console.WriteLine("---------------");
+        }
         static void Main(string[] args)
         {
+            ShowHUD();
+            TakeDamage();
+            ShowHUD();
+            Heal();
+            ShowHUD();
+            Console.ReadKey(true);
+
         }
     }
 }
